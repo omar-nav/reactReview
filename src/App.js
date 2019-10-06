43609c9c2176864c1bee220469ab76191d130c8c
omar-nav <omar.navarro@g.austincc.edu>
2019-10-06 11:25:07 -0500

    TODO add search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
+import MovieForm from "./components/movieForm";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navbar";
 import "./App.css";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
